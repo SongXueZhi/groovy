4d310725665c685d11c67b185082fcc38b1324cc
SongXueZhi <1010245182@qq.com>
2020-07-08 07:18:03 +0000

    add build jars

---
+++ target/resources/test/stubgenerator/circularLanguageReference/Rectangle.groovy
@@ -0,0 +1,14 @@
+package stubgenerator.circularLanguageReference;
+
+class Rectangle implements Shape {
+    private double x, y
+
+    Rectangle(double x, double y) {
+        this.x = x
+        this.y = y
+    }
+
+    double area() {
+        x * y
+    }
+}
